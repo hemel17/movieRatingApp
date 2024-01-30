c5c681cbdc7fa290dc5ad0a25ac711c79fa08105
Hemel17 <redewanmahmud@gmail.com>
2024-01-30 17:06:00 +0600

    main routes added

---
+++ src/pages/Rated/Rated.jsx
@@ -0,0 +1,6 @@
+const Rated = () => {
+  return (
+    <div>Rated</div>
+  )
+}
+export default Rated
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
+import Home from "../pages/Home/Home";
+import Rated from "../pages/Rated/Rated";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root></Root>
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+    {
+      path: '/rated',
+      element: <Rated></Rated>
+    }
+    ],
   },
 ]);
